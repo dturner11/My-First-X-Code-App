d7f465660fca5de29d4b5708fc45f669b0174bd0
David T <dturner11@gmail.com>
2018-01-12 10:09:27 +0000

    Initial Commit

---
+++ My First App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My First App
+//
+//  Created by David Turner on 12/01/2018.
+//  Copyright © 2018 DRT.Design. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
